02034cac08dbbad564dafd73f157775257d26263
Doug Hagey <douglas.hagey@gmail.com>
2020-03-02 17:00:36 +0100

    Merging all data tables into one big one

--- UnderstatScraper.py
+++ UnderstatScraper.py
@@ -5,14 +5,14 @@ Created on Mar 1 17:50:36 2020
 
 @author: doug hagey
 """
-from selenium import webdriver
-import time
+
 import pandas as pd
-from selenium.webdriver.firefox.options import Options
 import requests
-import ssl
 
 def scrape_understat(payload):
+    #Build request using url, headers (mimicking what Firefox does normally)
+    #Works best with verify=True as you won't get the ssl errors. Payload is 
+    #taylored for each request
     url = 'https://understat.com/main/getPlayersStats/'
     headers = {'content-type':'application/json; charset=utf-8',
     'Host': 'understat.com',
@@ -32,33 +32,40 @@ def scrape_understat(payload):
     json_player_data = inner_wrapper['players']
     return json_player_data
 
-def clean_df(player_df):
-    player_df.drop(['id', 'yellow_cards','red_cards', 'xGChain','xGBuildup'], axis=1, inplace=True)
+def clean_df(player_df, weeks):
+    # Get rid of the columns that we don't care about
+    player_df.drop(['yellow_cards','red_cards', 'xGChain','xGBuildup','games','time'], axis=1, inplace=True)
+    player_df  = player_df.rename(columns={'goals':'goals_'+weeks,'xG':'xG_'+weeks,'assists':'assists_'+weeks, 'xA':'xA_'+weeks, 'shots':'shots_'+weeks, 'key_passes':
+        'key_passes_'+weeks,'npg':'npg_'+weeks,'npxG':'npxG_'+weeks})
+    if weeks != '3wks':
+        player_df.drop(['position','team_title'], axis=1, inplace=True)
     return(player_df)
 
 #Create Pandas dataframes from each html table
 print('Getting data for last 3 matches')
 json_player_data = scrape_understat({'league':'EPL', 'season':'2019', 'n_last_matches': '3'})
 three_game_table = pd.DataFrame(json_player_data)
-three_game_df = clean_df(three_game_table)
+three_game_df = clean_df(three_game_table,'3wks')
 
 print('Getting data for the whole season')
 json_player_data = scrape_understat({'league':'EPL', 'season':'2019'})
 season_table = pd.DataFrame(json_player_data)
-season_df = clean_df(season_table)
+season_df = clean_df(season_table, 'season')
 
 print('Getting data for last 5 matches')
 json_player_data = scrape_understat({'league':'EPL', 'season':'2019', 'n_last_matches': '5'})
 five_game_table = pd.DataFrame(json_player_data)
-five_game_df = clean_df(five_game_table)
+five_game_df = clean_df(five_game_table, '5wks')
 
 print('Getting data for last 10 matches')
 json_player_data = scrape_understat({'league':'EPL', 'season':'2019', 'n_last_matches': '10'})
 ten_game_table = pd.DataFrame(json_player_data)
-ten_game_df = clean_df(ten_game_table)
+ten_game_df = clean_df(ten_game_table, '10wks')
 
-print('Writing csv files')
-season_df.to_csv('Understat_EPL_Player_Data_Season.csv', encoding='utf-8', index=False)
-three_game_df.to_csv('Understat_EPL_Player_Data_3Game.csv', encoding='utf-8', index=False)
-five_game_df.to_csv('Understat_EPL_Player_Data_5Game.csv', encoding='utf-8', index=False)
-ten_game_df.to_csv('Understat_EPL_Player_Data_10Game.csv', encoding='utf-8', index=False)
+print('Merging Tables')
+EPL_player_df = pd.merge(three_game_df, season_df, on=['id','player_name'])
+EPL_player_df = pd.merge(EPL_player_df, five_game_df, on=['id','player_name'])
+EPL_player_df = pd.merge(EPL_player_df, ten_game_df, on=['id','player_name'])
+
+print('Writing CSV File')
+EPL_player_df.to_csv('Understat_EPL_Player_Data_Combined.csv', encoding='utf-8', index=False)
