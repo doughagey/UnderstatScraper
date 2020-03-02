cb0c5f66bd94a8798c1323783a0706d945c9f59b
Doug Hagey <douglas.hagey@gmail.com>
2020-03-02 13:37:39 +0100

    Complete re-write using requests instead of selenium
    Ran into limitation where only top 10 were getting scraped with selenium so moved over to requests and built a valid request for each time period

--- UnderstatScraper.py
+++ UnderstatScraper.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Sun Nov 24 17:50:36 2019
+Created on Mar 1 17:50:36 2020
 
 @author: doug hagey
 """
@@ -9,56 +9,53 @@ from selenium import webdriver
 import time
 import pandas as pd
 from selenium.webdriver.firefox.options import Options
+import requests
+import ssl
 
-#use selenium to scrape player data from Understat.com using all 4 filter options for "games"
-try:
-    options = Options()
-    options.headless = True
-    driver = webdriver.Firefox(options=options)
-    driver.get('https://understat.com/league/EPL')
-    # Default Table = All
-    driver.find_element_by_xpath('/html/body/div[1]/div[3]/div[4]/div/div[1]/div[2]').click()
-    driver.find_element_by_xpath('//div[2]/div/ul/li[1]').click()
-    print('Getting Season Player Data')
-    season_playertable = driver.find_element_by_xpath('//*[@id="league-players"]').get_attribute('innerHTML')
-    time.sleep(5)
-    # Get data for last 3 games
-    driver.find_element_by_xpath('/html/body/div[1]/div[3]/div[4]/div/div[1]/div[2]').click()
-    driver.find_element_by_xpath('//div[2]/div/ul/li[2]').click()
-    time.sleep(5)
-    driver.find_element_by_xpath('/html/body/div[1]/div[3]/div[4]/div/div[1]/button/i').click()
-    time.sleep(5)
-    print('Getting Player Data For Last 3 Games')
-    three_game_playertable =driver.find_element_by_xpath('//*[@id="league-players"]').get_attribute('innerHTML')
-    # Get data for last 5 games
-    driver.find_element_by_xpath('/html/body/div[1]/div[3]/div[4]/div/div[1]/div[2]').click()
-    time.sleep(5)
-    driver.find_element_by_xpath('//div[2]/div/ul/li[3]').click()
-    time.sleep(5)
-    driver.find_element_by_xpath('/html/body/div[1]/div[3]/div[4]/div/div[1]/button/i').click()
-    time.sleep(5)
-    print('Getting Player Data For Last 5 Games')
-    five_game_playertable =driver.find_element_by_xpath('//*[@id="league-players"]').get_attribute('innerHTML')
-    # Get data for last 10 games
-    driver.find_element_by_xpath('/html/body/div[1]/div[3]/div[4]/div/div[1]/div[2]').click()
-    time.sleep(5)
-    driver.find_element_by_xpath('//div[2]/div/ul/li[4]').click()
-    time.sleep(5)
-    driver.find_element_by_xpath('/html/body/div[1]/div[3]/div[4]/div/div[1]/button/i').click()
-    time.sleep(5)
-    print('Getting Player Data For Last 10 Games')
-    ten_game_playertable =driver.find_element_by_xpath('//*[@id="league-players"]').get_attribute('innerHTML')
-    time.sleep(5)
-    driver.close()
-except Exception as e:
-    print('Encountered a problem')
-    driver.close()
+def scrape_understat(payload):
+    url = 'https://understat.com/main/getPlayersStats/'
+    headers = {'content-type':'application/json; charset=utf-8',
+    'Host': 'understat.com',
+    'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:73.0) Gecko/20100101 Firefox/73.0',
+    'Accept': 'application/json, text/javascript, */*; q=0.01',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+    'X-Requested-With': 'XMLHttpRequest',
+    'Content-Length': '39',
+    'Origin': 'https: // understat.com',
+    'Connection': 'keep - alive',
+    'Referer': 'https: // understat.com / league / EPL'
+    }
+    response = requests.post(url, data=payload, headers = headers, verify=True)
+    response_json = response.json()
+    inner_wrapper = response_json['response']
+    json_player_data = inner_wrapper['players']
+    return json_player_data
+
+def clean_df(player_df):
+    player_df.drop(['id', 'yellow_cards','red_cards', 'xGChain','xGBuildup'], axis=1, inplace=True)
+    return(player_df)
 
 #Create Pandas dataframes from each html table
-season_df = pd.read_html(season_playertable)[0]
-three_game_df = pd.read_html(three_game_playertable)[0]
-five_game_df = pd.read_html(five_game_playertable)[0]
-ten_game_df = pd.read_html(ten_game_playertable)[0]
+print('Getting data for last 3 matches')
+json_player_data = scrape_understat({'league':'EPL', 'season':'2019', 'n_last_matches': '3'})
+three_game_table = pd.DataFrame(json_player_data)
+three_game_df = clean_df(three_game_table)
+
+print('Getting data for the whole season')
+json_player_data = scrape_understat({'league':'EPL', 'season':'2019'})
+season_table = pd.DataFrame(json_player_data)
+season_df = clean_df(season_table)
+
+print('Getting data for last 5 matches')
+json_player_data = scrape_understat({'league':'EPL', 'season':'2019', 'n_last_matches': '5'})
+five_game_table = pd.DataFrame(json_player_data)
+five_game_df = clean_df(five_game_table)
+
+print('Getting data for last 10 matches')
+json_player_data = scrape_understat({'league':'EPL', 'season':'2019', 'n_last_matches': '10'})
+ten_game_table = pd.DataFrame(json_player_data)
+ten_game_df = clean_df(ten_game_table)
 
 print('Writing csv files')
 season_df.to_csv('Understat_EPL_Player_Data_Season.csv', encoding='utf-8', index=False)
